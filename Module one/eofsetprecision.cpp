13543e4b4de238c1bce523c9bc0d6504fca195c1
Saad Srabon <saadsrabon2@gmail.com>
2024-11-20 03:46:33 +0600

    Understanding of Eof

--- Module one/eofsetprecision.cpp
+++ Module one/eofsetprecision.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
+void pres (){
+    double x = 3.14159;
+    cout << setprecision(5) << x << endl;
+    cout << setprecision(9) << x << endl;
+    cout << setprecision(3) << x << endl;
+    cout << setprecision(2) << x << endl;
+    cout << setprecision(1) << x << endl;
+    cout <<fixed << setprecision(0) << x << endl;
+    cout << fixed << setprecision(3) << x << endl;
+}
 int main()
 {
     int x;
@@ -7,4 +18,10 @@ int main()
     {
         cout << x << " ";
     }
+
+   pres();
+    return 0;
 }
+
+// EOF means end of file
+// setprecision is used to set the numb
