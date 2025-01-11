cc84708ac501362c93946d9f8ee193a684529199
Saad Srabon <saadsrabon2@gmail.com>
2025-01-11 15:51:51 +0600

    files uploaded

---
+++ Dsa/createStackwithArray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myStackWithArray {
+    public:
+    int size =0;
+    int a[10];
+    void push(int val){
+        a[size] =val;
+        size++;
+    }
+
+
+};
+int main() 
+{
+    // 
+
+    int myArray = myStackWithArray()
+
+  
+    return 0;
+}
