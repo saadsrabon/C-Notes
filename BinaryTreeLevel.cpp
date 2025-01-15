44255fc2218eb9367ed0e6c7439e960cf011c11d
Saad Srabon <saadsrabon2@gmail.com>
2025-01-16 01:13:00 +0600

    Add new g

--- BinaryTreeLevel.cpp
+++ BinaryTreeLevel.cpp
@@ -14,15 +14,35 @@ using namespace std;
       }
 
    };
+
+   // print In level order
+   void print_level_order(Bnode *root){
+      queue<Bnode*>myDataQueue;
+      myDataQueue.push(root);
+
+      while(!myDataQueue.empty()){
+         Bnode *temp = myDataQueue.front();
+         myDataQueue.pop();
+         cout<< temp->val;
+         if(temp->left){
+            myDataQueue.push(temp->left);
+         }
+         if(temp->right){
+            myDataQueue.push(temp->right);
+         }
+      }
+   }
  
 int main() 
 {
    Bnode * root =new Bnode(1);
-    root->left=new Bnode(2);
-    root->right =new Bnode(3);
-    root->right->right =new Bnode(6);
-    root->left->left =new Bnode(4);
-    root->left->right =new Bnode(5);
+    root->left=new Bnode(3);
+    root->right =new Bnode(2);
+    root->right->right =new Bnode(4);
+    root->right->right->left =new Bnode(6);
+    root->right->right->right =new Bnode(5);
+
     
+    print_level_order(root);
     return 0;
 }
---
+++ BinaryTreeLevelInput.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left =NULL;
+        this->right =NULL;
+    };
+};
+
+// Take input for Level Order
+
+Node input_tree ()
+{
+    // First we need to create the root first
+}
+int main() 
+{
+    
+    return 0;
+}
