baae6d4479b7eb81a1e4e26545a7bba03e978ea9
Saad Srabon <saadsrabon2@gmail.com>
2025-01-16 02:10:57 +0600

    input tree enabled

--- BinaryTreeLevelInput.cpp
+++ BinaryTreeLevelInput.cpp
@@ -1,27 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     int val;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
     Node(int val)
     {
         this->val = val;
-        this->left =NULL;
-        this->right =NULL;
+        this->left = NULL;
+        this->right = NULL;
     };
 };
 
 // Take input for Level Order
 
-Node input_tree ()
+Node * input_tree()
 {
     // First we need to create the root first
+    int x;
+    cin >> x;
+    Node *root;
+    if (x == -1)
+        root = NULL;
+    else
+        root = new Node(x);
+    // Create a queue and push root there
+    queue<Node *> myData;
+    myData.push(root);
+
+    // Ber kore ante heb]
+    while (!myData.empty())
+    {
+        Node *p = myData.front();
+        myData.pop();
+        // For this dara i need to take two input
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+        {
+            myLeft = new Node(l);
+        }
+        if (r == -1)
+            myRight = NULL;
+        else
+        {
+            myRight = new Node(r);
+        }
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left)
+        {
+            myData.push(p->left);
+        }
+        if (p->right)
+        {
+            myData.push(p->right);
+        }
+    };
+    return root;
 }
-int main() 
+int main()
 {
+    Node * root =input_tree();
+
+
     
     return 0;
 }
