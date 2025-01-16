5fb205f94e1fe56beace01c412bc622088c32153
Saad Srabon <saadsrabon2@gmail.com>
2025-01-17 02:39:16 +0600

    problem solved

--- Nodelebvel.cpp
+++ Nodelebvel.cpp
@@ -22,3 +22,67 @@
 // 1
 // Explanation of Sample Input 1:
 //  For the first test case, the given binary tree is shown below.
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        bool isOriginal;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+        
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int nodeLevel(TreeNode<int>* root, int searchedValue)
+{
+    // Write your code here.
+    // First  amr value gula k pair kore nite hbe and traverse korte hbe 
+    //then oi time e check korte hbe is there is  a match or not?
+
+     queue<pair<TreeNode<int>* ,int>>q;
+      if(root){
+        q.push({root, 1});
+      }
+      else{
+          return 0;
+      }
+    
+    // Amra traverse korbo
+
+    while(!q.empty())
+    {
+        // Ber kore anbo
+        // oi  node niye kaaj korbo
+        //child gula k push korbo
+
+       pair<TreeNode <int> *,int> parent = q.front();
+       q.pop();
+
+       TreeNode<int> * newNode = parent.first;
+       int level = parent.second;
+
+       if(newNode->val ==searchedValue){
+           return level;
+       }
+
+       if(newNode->left){
+           q.push({newNode->left ,level+1});
+       }
+       if(newNode->right){
+           q.push({newNode->right ,level+1});
+       }
+    };
+
+}
