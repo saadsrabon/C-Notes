1eccc62a1ec31602910537ffd64a28dbd74aea4a
Saad Srabon <saadsrabon2@gmail.com>
2025-01-17 02:26:12 +0600

    question asigned

---
+++ Nodelebvel.cpp
@@ -0,0 +1,24 @@
+// Problem statement
+// Ninja has been given a binary tree having N nodes and an integer M, he needs to find the level of node M. He finds it difficult to solve and asks for your help.
+
+// Note:
+
+// Consider root to be at level 1. It is guaranteed that all the nodes in the binary tree have distinct values.
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints:
+// 1 <= T <= 5
+// 0 <= N <= 3000
+// 0 <= node data <= 10^9  
+
+// Time Limit: 1 sec
+// Sample Input 1:
+// 2
+// 1 2 3 -1 -1 -1  6 -1 -1
+// 6
+// 1 2 -1 -1 -1
+// 1
+// Sample Output 1:
+// 3
+// 1
+// Explanation of Sample Input 1:
+//  For the first test case, the given binary tree is shown below.
