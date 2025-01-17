b9f555abed59098f3e4d2732bcb3b034f58e97c1
Saad Srabon <saadsrabon2@gmail.com>
2025-01-17 15:38:23 +0600

    decesnding order

---
+++ Leaf_Nodes.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// Function to input the binary tree
+Node* input_tree() {
+    int x;
+    cin >> x;
+    Node* root;
+    if (x == -1)
+        root = NULL;
+    else
+        root = new Node(x);
+
+    queue<Node*> myData;
+    myData.push(root);
+
+    while (!myData.empty()) {
+        Node* p = myData.front();
+        myData.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left) {
+            myData.push(p->left);
+        }
+        if (p->right) {
+            myData.push(p->right);
+        }
+    }
+    return root;
+}
+
+// Function to collect all leaf nodes
+void collect_leaf_nodes(Node* root, vector<int>& leafNodes) {
+    if (root == NULL) return;
+
+    if (!root->left && !root->right) {
+        leafNodes.push_back(root->val);
+    }
+    collect_leaf_nodes(root->left, leafNodes);
+    collect_leaf_nodes(root->right, leafNodes);
+}
+
+int main() {
+    Node* root = input_tree();
+
+
+    vector<int> leafNodes;
+
+
+    collect_leaf_nodes(root, leafNodes);
+
+
+    sort(leafNodes.rbegin(), leafNodes.rend());
+
+ 
+    for (int val : leafNodes) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
